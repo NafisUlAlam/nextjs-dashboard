b4bdaa41d6401de046f52a0e61789aa1659ee20d
Nafis <ahmednafis74@gmail.com>
2025-07-01 22:36:31 +0600

    added loading.tsx file for dashboard

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DashboardSkeleton from "../ui/skeletons";
+
+const Loading = () => {
+  console.log("loading for dashboard");
+  return <DashboardSkeleton></DashboardSkeleton>;
+};
+
+export default Loading;
