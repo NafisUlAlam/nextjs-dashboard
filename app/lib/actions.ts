30073db5b967c29d40d6df56f6df3f3ef84bd12c
Nafis <ahmednafis74@gmail.com>
2025-08-19 20:47:42 +0600

    useActionState hook used for server side form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,25 +9,46 @@ import { z } from "zod";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
+  customerId: z.string({ invalid_type_error: "Select a customer" }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Amount must be greater than $0" }),
   date: z.string(),
-  status: z.enum(["pending", "paid"]),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Select an invoice status",
+  }),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formdata: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formdata: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formdata.get("customerId"),
     amount: formdata.get("amount"),
     status: formdata.get("status"),
   });
+  console.log(validatedFields.error?.flatten().fieldErrors);
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing fields, failed to create invoice",
+    };
+  }
+  const { amount, customerId, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-  //console.log(date);
+
   try {
     await sql`insert into invoices (customer_id, amount, status, date) 
     values (${customerId}, ${amountInCents}, ${status}, ${date})`;
